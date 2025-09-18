23b8b518a02e12d0d85ae84776dfc4ccc1d1ece4
perezrepisoraul-cloud <perezrepisoraul@gmail.com>
2025-09-18 12:13:35 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// No es necesario crear la funcion main para JS 
+// function main(){
+
+    document.getElementById("h11").style.backgroundColor = "red";
+    document.getElementById("h11").style.color = "white";
+
+// }
+
+// main();
+
+// Implementación de funciones
+/************************* FUNCTIONS *******************/
