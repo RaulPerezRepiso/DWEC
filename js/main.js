4c969e25a8faeccd47d2d6b8e13dde3ebf87fa00
perezrepisoraul-cloud <perezrepisoraul@gmail.com>
2025-09-23 11:11:50 +0200

    DWEC

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 // No es necesario crear la funcion main para JS 
 // function main(){
 
-    document.getElementById("h11").style.backgroundColor = "red";
-    document.getElementById("h11").style.color = "white";
+var name2 = "Jhon";
+
+console.log("Nombre es: " + name2+ " Tipo: " + typeof name2);
+
+var name2 = 7;
+
+console.log("Nombre es: " + name2+ " Tipo: " + typeof name2);
+
+var name2 = true;
+
+console.log("Nombre es: " + name2+ " Tipo: " + typeof name2);
+
+var name2 = [5,6,7,8];
+
+console.log("Nombre es: " + name2+ " Tipo: " + typeof name2);
+
+var name2 = -1/0;
+
+console.log("Nombre es: " + name2+ " Tipo: " + typeof name2);
+
+    // document.getElementById("h11").style.backgroundColor = "red";
+    // document.getElementById("h11").style.color = "white";
 
 // }
 
