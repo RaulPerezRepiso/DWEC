c42e069867f87de14a7143a08962a7eec801d94f
perezrepisoraul-cloud <perezrepisoraul@gmail.com>
2025-09-30 13:08:04 +0200

    Bucles, Spread Y Rest

--- Pruebas/js/main.js
+++ Pruebas/js/main.js
@@ -149,9 +149,11 @@ console.log(myArray); */
 // document.getElementById("pre1").innerHTML = "Hola , \n¿Como estás?";
 // document.getElementById("ta1").innerHTML = "Hola , \n¿Como estás?";
 
+// Variables declaradas
 let h11 = document.getElementById("h11");
 let b1 = document.getElementById("b1");
 let it1 = document.getElementById("it1");
+let b2 = document.getElementById("b2");
 
 // let a = 5;
 // let b = 10;
@@ -236,12 +238,12 @@ function pressButtonB1(){
 // const i = 0;
 
 // Bucles For
-/* let myArray = [1,2,3,4,5,6,7,8,9,10];
+// let myArray = [1,2,3,4,5,6,7,8,9,10];
 
-for (let i=0; i<myArray.length; i++){
+/* for (let i=0; i<myArray.length; i++){
     h11.innerHTML += myArray[i] + ", ";
-} */
-
+}
+ */
 // Bucles For in (Dentro de bucles como el for in las variables no serán super globales)
 /* let myArray = [1,2,3,4,5,6,7,8,9,10];
 
@@ -249,15 +251,28 @@ for(i in myArray){
     h11.innerHTML += myArray[i] + ", ";
 }*/
 
-/* let myObj = {
+let myObj = {
     nombre: "Pepe",
     edad: 34,
     email: "pepe@gmail.es"
 }
 
-for(i in myObj){
+/* for(i in myObj){
     h11.innerHTML += myObj[i] + ", ";
 } */
+
+// elem es la variable determinada para el for of aunque podemos usar otras
+// Lo que especificamos a la izquierda del of la propia variable a mostrar menos
+// Dedicad casi siempre para Arrays
+/* for(elem of myArray){
+    h11.innerHTML += elem + ", ";
+} */
+
+// Para caso de array de objetos puede devolver valores predeterminados de una array 
+/* for(elem of myObj){
+    h11.innerHTML += elem.nombre + ", ";
+} */
+
 
 // Uso del switch con funciones
 /* b1.onclick = sumar10;
@@ -282,3 +297,140 @@ function sumar10() {
             h11.innerHTML += parseFloat(it1.value) + 10;
     }
 } */
+
+// Sentencias Break & Continue
+// let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//Continue para mostrar los impares
+/* for (let i=0; i<myArray.length; i++){
+    if ((myArray[i]%2)==0)
+        continue;
+    else
+    h11.innerHTML += myArray[i] + ", ";
+} */
+
+// Break para mostrar el primer numero par que encuentre y se salga
+/* for (let i=0; i<myArray.length; i++){
+    if ((myArray[i]%2)==0)
+        break;
+    h11.innerHTML += myArray[i] + ", ";
+} */
+
+/* let result = multiplica(3, 5);
+h11.innerHTML = result; */
+
+// b2.onclick = multiplicaButton;
+
+// Función para coger dos valores de los text y hacer la multiplicación al clickar en el boton se mostrar en otro text 
+var itop1 = document.getElementById("itop1");
+var itop2 = document.getElementById("itop2");
+
+/* function multiplicaButton() {
+    let result = 0;
+    if (itop1.value == "")
+        result = multiplica(parseFloat(itop1.value));
+    else if (itop2.value == "")
+        result = multiplica(parseFloat(itop2.value));
+    else
+        result = multiplica(itop1.value * itop2.value);
+    document.getElementById("it1").value = result;
+}
+ */
+/* function multiplica(op1, op2) {
+    let result = 0;
+    
+    if ((!op1) && (!op2)) {
+        alert("Debe introducir al menos un valor!!!")
+    }
+    else {
+        let op1_dentreo = op1 || 1;
+        let op2_dentreo = op2 || 1;
+
+        result = op1_dentreo * op2_dentreo;
+    }
+
+    return result;
+} */
+
+// Multiplica en cascada tantos x elementos de demos gracis a argumemts
+// multiplica(2,3,5,6,7);
+
+/* function multiplica() {
+    let result = 1;
+    
+    for(op of arguments){
+        result *= op;
+    }
+
+    h11.innerHTML = result;
+}  */
+
+/* function multiplica() {
+    let result = 1;
+    
+    for (let i = 0; i < arguments.length; i++) {
+        result *= arguments[i];
+    }
+
+    h11.innerHTML = result;
+}  */
+
+/* function multiplica() {
+    let result = 1;
+    
+    for(i in arguments){
+        result *= arguments[i];
+    }
+
+    h11.innerHTML = result;
+}  */
+
+// Parámetro REST
+/* function multiplica(op1,op2, ...rest){
+    let result = 1;
+
+    console.log(rest);
+
+    for(i of rest){
+        result *= i;
+    }
+
+    h11.innerHTML = result;
+}
+ */
+// Parametro Spread
+let myArray = [true, 5, 7, 9];
+
+/**
+ * @returns Devuelve la media de las notas si el primer parámetro 
+ * es un boolean a true. En caso contrario, devuelve "Suspenso"
+ */
+calculaMedia(...myArray);
+
+/* function calculaMedia(op1, ...rest) {
+    let result = 0;
+
+    if (op1 == true) {
+        for (i of rest) {
+            result += i / rest.length;
+        }
+        h11.innerHTML = "Nota media: " +result;
+
+    } else
+        h11.innerHTML = "Suspenso";
+} */
+
+function calculaMedia() {
+    if ((typeof arguments[0] == "boolean") && (arguments[0])) {
+        let result = 0;
+        for (i = 1; i < arguments.length; i++)
+            if (typeof arguments[i] == "number")
+                result += arguments[i];
+
+        result /= arguments.length - 1;
+
+        h11.innerHTML = "Nota media: " + result;
+    }
+    else
+        h11.innerHTML = "Nota media: Suspenso";
+}
