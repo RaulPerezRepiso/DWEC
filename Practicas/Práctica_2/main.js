807c79abdc161bb7d0f5a2e493c16b75dff35d5f
perezrepisoraul-cloud <perezrepisoraul@gmail.com>
2025-10-14 14:47:50 +0200

    Comienzo Práctica 2. 14/10/2025

---
+++ Practicas/Práctica_2/main.js
@@ -0,0 +1,91 @@
+/*Ejercicio 1 */
+// Declaracion de variables
+let persona = document.getElementById("persona");
+let profesor = document.getElementById("profesor");
+let estudiante = document.getElementById("estudiante")
+
+class Persona {
+    constructor(nombre, edad, genero) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.genero = genero;
+    }
+}
+
+ObjDetalles = () => {
+    Persona.nombre = "Pepe";
+    Persona.edad = 34;
+    Persona.genero = "Hombre";
+
+    return "Nombre: " + Persona.nombre + ". Edad: " + Persona.edad + " y género " + Persona.genero;
+}
+
+persona.innerHTML = ObjDetalles();
+
+class Estudiante extends Persona {
+    constructor(nombre, edad, genero, curso, grupo) {
+        super();
+        this.curso = curso;
+        this.grupo = grupo;
+    }
+}
+
+registrar = () => {
+    Estudiante.curso = "2";
+    Estudiante.grupo = "A"
+
+    return ObjDetalles() + " Curso " + Estudiante.curso + " y grupo " + Estudiante.grupo;
+}
+
+estudiante.innerHTML = registrar();
+
+class Profesor extends Persona {
+    constructor(nombre, edad, genero, asignatura, nivel) {
+        super();
+        this.asignatura = asignatura;
+        this.nivel = nivel;
+    }
+}
+
+asignar = () => {
+    Profesor.asignatura = "Matemáticas";
+    Profesor.grnivelupo = "2"
+
+    return ObjDetalles() + " Imparte " + Profesor.asignatura + " en el Curso " + Profesor.nivel;
+}
+
+profesor.innerHTML = asignar();
+
+/*Ejercicio 2 */
+
+/*Ejercicio 3 */
+
+/*Ejercicio 4 */
+
+/*Ejercicio 5 */
+
+/*Ejercicio 6 */
+
+/*Ejercicio 7 */
+
+/*Ejercicio 8 */
+
+/*Ejercicio 9 */
+
+/*Ejercicio 10 */
+
+/*Ejercicio 11 */
+
+/*Ejercicio 12 */
+
+/*Ejercicio 13 */
+
+/*Ejercicio 14 */
+
+/*Ejercicio 15 */
+
+/*Ejercicio 16 */
+
+/*Ejercicio 17 */
+
+/*Ejercicio 18 */
