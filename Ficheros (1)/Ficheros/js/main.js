5a9b857ba36574478fbbb384d0ab76f4d02974f2
perezrepisoraul-cloud <perezrepisoraul@gmail.com>
2026-01-16 11:02:36 +0100

    Examen Vacío. 16/01/2026

---
+++ Ficheros (1)/Ficheros/js/main.js
@@ -0,0 +1,60 @@
+// Array con las comidas del menú principal:
+var comidas = new Array();
+
+var entrantes = [
+    {entrante: "Turrón salada de queso y frutos secos"},
+    {entrante: "Piruletas crujientes de parmesano"},
+    {entrante: "Hummus con remolacha"},
+    {entrante: "Mejillones a la cerveza con bacon"},
+    {entrante: "Croquetas de atún"},
+    {entrante: "Quesadilla de chorizo criollo con aguacate"},
+    {entrante: "Ensalada de endivias, surimi y salsa roquefort"},
+    {entrante: "Guacamole"},
+    {entrante: "Pan casero con aceite de oliva y sal negra en escamas"},
+    {entrante: "Verduras rellenas de arroz"}
+];
+
+comidas.push(entrantes);
+
+var comidaCasera = [
+    {casera: "Sopa de pollo"},
+    {casera: "Sopa de verdura"},
+    {casera: "Crema de calabaza"},
+    {casera: "Arroz con pollo"},
+    {casera: "Paella de mariscos"},
+    {casera: "Arroz caldoso de pescado"},
+    {casera: "Pollo al horno"},
+    {casera: "Conejo al ajillo"},
+    {casera: "Potaje de lentejas o abichuelas"},
+    {casera: "Pescado al horno"}
+];
+
+comidas.push(comidaCasera);
+
+var comidaOriental = [
+    {oriental: "Ramen"},
+    {oriental: "Rollitos de primavera vegetales"},
+    {oriental: "Atún con arroz"},
+    {oriental: "Wok de verduras y fideos chinos"},
+    {oriental: "Arroz al estilo oriental"},
+    {oriental: "Nishime de verduras"},
+    {oriental: "Solomillo de cerdo agridulce"},
+    {oriental: "Noodles de arroz con gambas"},
+    {oriental: "Rollitos de alga nori con con verduras en tempura"}
+];
+
+comidas.push(comidaOriental);
+
+var mediterranea = [
+    {medi: "Judías verdes estofadas"},
+    {medi: "Gazpacho"},
+    {medi: "Magro de cerdo con champiñón"},
+    {medi: "Salteado de garbanzos con cebolla, guisantes y tomate"},
+    {medi: "Lasaña de espinacas"},
+    {medi: "Berenjena al horno"},
+    {medi: "Dorada a la plancha"},
+    {medi: "Pizza mediterránea"},
+    {medi: "Tortilla de pimiento y cebolla"}
+];
+
+comidas.push(mediterranea);
