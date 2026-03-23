9f9494b24fa657fb6c293210945fd7950f7b4061
Practicas <raulperezrepiso@gmail.com>
2026-03-23 13:09:03 +0100

    Estilos básicas subidos. 23/03/2026

--- 3ºTrimestre/PF/js/main.js
+++ 3ºTrimestre/PF/js/main.js
@@ -1,23 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-    console.log("JS cargado correctamente");
-
-    fetch("https://jsonplaceholder.typicode.com/todos")
-        .then(res => {
-            console.log("Estado de la respuesta:", res.status);
-            return res.json();
-        })
-        .then(data => {
-            console.log("Datos recibidos:", data);
-
-            const random = Math.floor(Math.random() * data.length);
-            const habito = data[random].title;
-
-            document.getElementById("habito-dia").textContent = habito;
-        })
-        .catch(err => {
-            console.error("ERROR FETCH:", err);
-            document.getElementById("habito-dia").textContent = "Error cargando hábito";
-        });
-
+document.getElementById("login-btn").addEventListener("submit", function(e) {
+    e.preventDefault();
+    window.location.href = "login.html";
 });
